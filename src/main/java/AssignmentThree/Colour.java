0a9dd3a782bd58d8b9cb35e0e8e64ab69c11f67c
Mary Holmes <118381376@umail.ucc.ie>
2020-12-16 20:38:22 +0000

    Created initial values and set them

--- src/main/java/AssignmentThree/Colour.java
+++ src/main/java/AssignmentThree/Colour.java
@@ -2,4 +2,9 @@ package AssignmentThree;
 
 public class Colour {
 
+    private String model = "RGB";
+    private int componentOne = 0;
+    private int componentTwo = 0;
+    private int componentThree = 0;
+
 }
