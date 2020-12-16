b103fb3d5616207200167410e8d921444e8c10e6
Mary Holmes <118381376@umail.ucc.ie>
2020-12-16 20:45:50 +0000

    Created the acceptIfSatisfiesRange(). This method passes in an object of the Colour class, called TEST_COLOUR. The aim of this method is to check whether or not the values of each of the three colour components are within the range (0 to 255). As expected, this method failed the test, as the method in the Colour class, satisfiesRange(TEST_COLOUR) has not yet been implemented.

--- src/test/java/AssignmentThree/ColourTest.java
+++ src/test/java/AssignmentThree/ColourTest.java
@@ -1,7 +1,15 @@
 package AssignmentThree;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColourTest {
+
+    @Test
+    public void acceptIfSatisfiesRange() {
+        Colour TEST_COLOUR = new Colour("Grey", 153, 153, 153);
+        assertTrue(Colour.satisfiesRange(TEST_COLOUR));
+    }
 
 }
